020802b9e152946bd7d75d1d9dc6dd885d6a8a2b
Mikko Kutilainen <mikko@meiko.fi>
2015-12-05 11:06:06 +0200

    Initial Commit

---
+++ SHMenu/SHMenu.h
@@ -0,0 +1,19 @@
+//
+//  SHMenu.h
+//  SHMenu
+//
+//  Created by Mikko Kutilainen on 5.12.2015.
+//  Copyright © 2015 Shingle Oy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHMenu.
+FOUNDATION_EXPORT double SHMenuVersionNumber;
+
+//! Project version string for SHMenu.
+FOUNDATION_EXPORT const unsigned char SHMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHMenu/PublicHeader.h>
+
+
