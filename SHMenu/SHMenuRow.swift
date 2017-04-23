c2da52085af88cadb55b673fcb49116e60eba29b
mikkokut <mikko93@gmail.com>
2017-04-23 14:03:23 +0300

    deployment target to 9.0

--- SHMenu/SHMenuRow.swift
+++ SHMenu/SHMenuRow.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 open class SHMenuRow {
     var cell: ((UITableView) -> (UITableViewCell))
--- SHMenu/SHMenuSection.swift
+++ SHMenu/SHMenuSection.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 open class SHMenuSection {
     var rows: [SHMenuRow]
